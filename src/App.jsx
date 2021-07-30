11d50ced33249462d8b3e24ee778ad00893d480c
abr-ya <gamer05@list.ru>
2021-07-30 16:14:03 +0300

    feat: Add and config Jest and React Testing Library

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,50 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!!!</p>
-        <p>
-          <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
-            count is:
-            {' '}
-            {count}
-          </button>
-        </p>
-        <p>
-          Edit
-          {' '}
-          <code>App.jsx</code>
-          {' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1 className="app-header">Hello from Vite + React!</h1>
+      <p>
+        <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
+          Add 1 to counter
+        </button>
+      </p>
+      <p>
+        Counter value is:
+        {' '}
+        {count}
+      </p>
+      <p>
+        Edit
+        {' '}
+        <code>App.jsx</code>
+        {' '}
+        and save to test HMR updates.
+      </p>
+      <p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {' | '}
+        <a
+          className="App-link"
+          href="https://vitejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vite Docs
+        </a>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
