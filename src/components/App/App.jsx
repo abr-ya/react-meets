bc9fc7703562d39525fbd4fa7225422750913976
abr-ya <gamer05@list.ru>
2021-08-01 12:02:40 +0300

    feat: Add Navigation Bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,9 +4,11 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from '../../pages/AllMeetups';
 import NewMeetupPage from '../../pages/NewMeetup';
 import FavoritesPage from '../../pages/Favorites';
+import MainNavigation from '../../layouts/MainNavigation';
 
 const App = () => (
   <div>
+    <MainNavigation />
     <Switch>
       <Route path="/" exact>
         <AllMeetupsPage />
