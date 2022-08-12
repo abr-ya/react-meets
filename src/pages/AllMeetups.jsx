66c35b16a5651013eb639a639b6c8e2fa3426540
abr-ya <gamer05@list.ru>
2022-08-12 08:50:46 +0300

    feat: Show meets from Firebase

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
+const AllMeetupsPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-const AllMeetupsPage = () => (
-  <section>
-    <h1>All Meetups</h1>
-    <MeetupList meetups={DUMMY_DATA} />
-  </section>
-);
+  useEffect(() => {
+    fetch(
+      'https://react-meets-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+    )
+      .then((res) => res.json()).then((json) => {
+        const meetaps = Object.keys(json).map((id) => ({ id, ...json[id] }));
+        setData(meetaps);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>loading ...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={data} />
+    </section>
+  );
+};
 
 export default AllMeetupsPage;
